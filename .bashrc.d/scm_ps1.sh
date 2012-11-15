f0b603779b0b5426eb4d5fa61d35018694020ce1
Huiming Teo <teohuiming@gmail.com>
2012-11-15 18:51:09 +0800

    add comment in scm_ps1

--- .bashrc.d/scm_ps1.sh
+++ .bashrc.d/scm_ps1.sh
@@ -1,6 +1,10 @@
 # Simple svn & git bash prompt
 # source: http://www.codeography.com/2009/05/26/speedy-bash-prompt-git-and-subversion-integration.html
 # see also: http://superuser.com/questions/288438/bash-completion-for-commands-in-mac-os
+#
+# Dependencies:
+#   brew install git              # to get __git_ps1 in git_completion.sh
+#   brew install bash-completion  # to load git_completion.sh
 scm_ps1() {
     local s=
     if [[ -d ".svn" ]] ; then
