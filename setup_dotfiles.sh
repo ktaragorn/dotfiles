69582fb5f4e0e6371c53de3cb2a36cf7de2de246
Karthik T <karthikt.holmes+github@gmail.com>
2014-01-25 19:10:42 +0800

    Force links in setup script Misc Sublime update

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -2,20 +2,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #gitconfig
 rm ../.gitconfig
-ln -sn $DIR/gitconfig $DIR/../.gitconfig
+ln -sfn $DIR/gitconfig $DIR/../.gitconfig
 
 #bashrc
 echo ". ~/dotfiles/bashrc.d/source_all" >> ~/.bashrc
 ln ~/dotfiles/bashrc.d/inputrc ~/.inputrc
 
 #byobu specific
-ln -sn $DIR/tmux.conf $DIR/../.byobu/.tmux.conf
+ln -snf $DIR/tmux.conf $DIR/../.byobu/.tmux.conf
 
 #rdebug
-ln -sn ~/dotfiles/rdebugrc ~/.rdebugrc
+ln -snf ~/dotfiles/rdebugrc ~/.rdebugrc
 
 #vim
-ln -sn ~/dotfiles/vimrc ~/.vimrc
+ln -snf ~/dotfiles/vimrc ~/.vimrc
 
 #ngrok
-ln -sn ~/dotfiles/ngrok ~/.ngrok
+ln -sfn ~/dotfiles/ngrok ~/.ngrok
