c8f985924e0f918ff1ccfb6bf6d89c04cb6bcc10
Karthik T <karthikt.holmes+github@gmail.com>
2015-12-23 15:42:07 +0800

    Add code to make sure path additions are once only, and duplicates are avoided
    Remove logic to check for tux

---
+++ bashrc.d/path.sh
@@ -0,0 +1,20 @@
+# to make sure we only add to PATH once, for
+#http://stackoverflow.com/questions/13058578/how-to-prevent-tmux-from-filling-up-the-global-path-variable-with-duplicated-pat
+
+pathappend() {
+  for ARG in "$@"
+  do
+    if [ -d "$ARG" ] && [[ ":$PATH:" != *":$ARG:"* ]]; then
+        PATH="${PATH:+"$PATH:"}$ARG"
+    fi
+  done
+}
+
+pathprepend() {
+  for ARG in "$@"
+  do
+    if [ -d "$ARG" ] && [[ ":$PATH:" != *":$ARG:"* ]]; then
+        PATH="$ARG${PATH:+":$PATH"}"
+    fi
+  done
+}
