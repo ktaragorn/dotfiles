fded8e7d6b3553fcaccf620f0cd7d5fe5e241bce
Karthik T <karthikt.holmes+github@gmail.com>
2018-02-03 14:11:40 +0800

    Move flexget to use schedule
    Check space left based on MB, alert below 1000 MB

--- setup_ansible/pine/files/monitor_free_disk_space.sh
+++ setup_ansible/pine/files/monitor_free_disk_space.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-CURRENT=$(df $1 | grep / | awk '{ print $5}' | sed 's/%//g')
+CURRENT=$(df -BM $1 | grep / | awk '{ print $4}' | sed 's/M//g')
 THRESHOLD="$2"
 
-if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
-    /home/ubuntu/pushbullet "Disk Space Alert" "Your partition, $1, has critically low remaining free space. Used: $CURRENT percent"
+if [ "$CURRENT" -lt "$THRESHOLD" ] ; then
+    /home/ubuntu/pushbullet "Disk Space Alert" "Your partition, $1, has critically low remaining free space. Remaining: $CURRENT MB"
 fi
