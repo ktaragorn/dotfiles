e3360aacddd7913695e45f7b0f0b662001962257
Karthik T <karthikt.holmes+github@gmail.com>
2018-11-06 21:09:06 +0800

    Fix chest monitoring script

--- setup_ansible/pine/files/monitor_chest_drive.sh
+++ setup_ansible/pine/files/monitor_chest_drive.sh
@@ -4,11 +4,10 @@ ls
 
 if [ $? -eq 0 ]; then
     echo OK
-elif [$1 -eq "try_only"]; then
+elif [ $1 = 'try_only' ]; then
 	/home/ubuntu/pushbullet "Chest HDD Alert" "The Chest hard disk is down, and unable to fix by remount!"
 else
     /home/ubuntu/pushbullet "Chest HDD Alert" "The Chest hard disk is down, attempting to remount!"
-    sudo umount /media/ubuntu/Chest/
-    sudo mount /media/ubuntu/Chest/
+    sudo umount /media/ubuntu/Chest/ && sleep 5 && sudo mount /media/ubuntu/Chest/
     /home/ubuntu/monitor_chest_drive.sh try_only
 fi
