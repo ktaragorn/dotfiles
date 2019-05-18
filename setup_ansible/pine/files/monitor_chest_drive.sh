bfb569de81edd9f280c9e0ab309dabe3492e6c46
Karthik T <karthikt.holmes+github@gmail.com>
2019-05-18 19:25:24 +0800

    Improve deluge restarting and chest remounting
    deluge-web will restart with deluged stoping or restarting
    when remounting chest, also restart deluged

--- setup_ansible/pine/files/monitor_chest_drive.sh
+++ setup_ansible/pine/files/monitor_chest_drive.sh
@@ -8,7 +8,7 @@ elif [ $1 = 'try_only' ]; then
 	/home/ubuntu/pushbullet "Chest HDD Alert" "The Chest hard disk is down, and unable to fix by remount!"
 else
     /home/ubuntu/pushbullet "Chest HDD Alert" "The Chest hard disk is down, attempting to remount!"
-    sudo systemctl stop smbd && sudo umount /media/ubuntu/Chest/ && sleep 5 && sudo mount /media/ubuntu/Chest/
-    sudo systemctl start smbd
+    sudo systemctl stop smbd && sudo systemctl stop deluged && sudo umount /media/ubuntu/Chest/ && sleep 5 && sudo mount /media/ubuntu/Chest/
+    sudo systemctl start smbd && sudo systemctl start deluged
     /home/ubuntu/monitor_chest_drive.sh try_only
 fi
