7ba481a4a701a40c6697a06fd48c7b7982e5424d
Karthik T <karthik@ridebeam.com>
2019-06-21 13:35:41 +0800

    Try moving remounting into monit
    Create a separate script to remount and add that to monit the cron script

--- setup_ansible/pine/files/monitor_chest_drive.sh
+++ setup_ansible/pine/files/monitor_chest_drive.sh
@@ -7,8 +7,6 @@ if [ $? -eq 0 ]; then
 elif [ $1 = 'try_only' ]; then
 	/home/ubuntu/pushbullet "Chest HDD Alert" "The Chest hard disk is down, and unable to fix by remount!"
 else
-    /home/ubuntu/pushbullet "Chest HDD Alert" "The Chest hard disk is down, attempting to remount!"
-    sudo systemctl stop smbd && sudo systemctl stop deluged && sudo umount /media/ubuntu/Chest/ && sleep 5 && sudo mount /media/ubuntu/Chest/
-    sudo systemctl start smbd && sudo systemctl start deluged
+    /home/ubuntu/remount_chest_drive.sh
     /home/ubuntu/monitor_chest_drive.sh try_only
 fi
