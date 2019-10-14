4f723159fe32b4762768907167d839b6627e3943
Karthik T <karthikt.holmes+github@gmail.com>
2019-10-14 14:43:08 +0800

    Incomplete platformio setup for nodemcu controller
    Move old code into this and replace wifi creds with vars

--- setup_ansible/ac_controller/src/main.ino
+++ setup_ansible/ac_controller/src/main.ino
@@ -50,8 +50,8 @@
 #include <pins_arduino.h>
 #include <OneButton.h>
 
-const char* kSsid = "ironforge";
-const char* kPassword = "speakfriendandenter";
+const char* kSsid = {{wifi_ssid}};
+const char* kPassword = {{wifi_password}};
 MDNSResponder mdns;
 
 #if defined(ESP8266)
@@ -75,7 +75,7 @@ IRrecv irrecv(kRecvPin);
 void sendDaikinOnOffCommand(bool on, int temperature = 25, int fan = 3){
   IRDaikinESP ac(kIrLed);
   ac.begin();
-  
+
   if(on){
     ac.on();
   }else{
@@ -93,7 +93,7 @@ void sendDaikinOnOffCommand(bool on, int temperature = 25, int fan = 3){
 void sendAcOn(){
   const String temp = server.hasArg("temperature") ? server.arg("temperature") : "25";
   const String fan = server.hasArg("fan") ? server.arg("fan") : "3";
-  
+
   sendDaikinOnOffCommand(true, temp.toInt(), fan.toInt());
   Serial.println("Sent turn on command with temperature = " + temp + " and fan = " + fan);
 }
@@ -235,17 +235,17 @@ void trigger_homeassistant_webhook(String webhook, String value){
   }
 }
 
-void on_click(){  
+void on_click(){
    Serial.println("Button Pressed");
    trigger_homeassistant_webhook("button_pressed", "on");
 }
 
-void on_double_click(){  
+void on_double_click(){
    Serial.println("Button double Pressed");
    trigger_homeassistant_webhook("button_double_pressed", "on");
 }
 
-void on_long_press(){  
+void on_long_press(){
    Serial.println("Button long Pressed");
    trigger_homeassistant_webhook("button_long_pressed", "on");
 }
@@ -273,7 +273,7 @@ void setup(void) {
   Serial.begin(115200);
 
   irrecv.enableIRIn();  // Start the receiver
-  
+
   wifi_setup();
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
