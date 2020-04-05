9deccae07cbd4d096a346b95cfb9a170703c3769
Karthik T <karthikt.holmes+github@gmail.com>
2020-04-05 13:07:59 +0800

    Misc logs fixes
    And trigger notification tells which trigger triggered

--- setup_ansible/ac_controller/src/homeassistant_webhook.cpp
+++ setup_ansible/ac_controller/src/homeassistant_webhook.cpp
@@ -8,14 +8,14 @@ void trigger_homeassistant_webhook(String webhook, String value){
   HTTPClient http;
 
   if (http.begin(client, "http://hass.middle.earth/api/webhook/" + webhook)) {  // HTTP
-    util_log("[HTTP] GET...\n");
+    util_log("[HTTP] Webhook Start");
     // start connection and send HTTP header
     int httpCode = http.POST(value);
 
     // httpCode will be negative on error
     if (httpCode > 0) {
       // HTTP header has been send and Server response header has been handled
-      util_log("[HTTP] POST... code: " + httpCode);
+      util_log("[HTTP] POST... code: " + String(httpCode));
 
       // file found at server
       if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
@@ -28,6 +28,6 @@ void trigger_homeassistant_webhook(String webhook, String value){
 
     http.end();
   } else {
-    util_log("[HTTP} Unable to connect");
+    util_log("[HTTP] Unable to connect");
   }
 }
