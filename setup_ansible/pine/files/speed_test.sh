16504951fc3286bf6e1b5e2d9ac9b0585ebc1b1f
Karthik T <karthikt.holmes+github@gmail.com>
2021-01-02 15:40:29 +0800

    Fix speed test script

--- setup_ansible/pine/files/speed_test.sh
+++ setup_ansible/pine/files/speed_test.sh
@@ -1,5 +1,6 @@
-speed=$(($(iperf3 -c 192.168.1.1 -J | jq .end.sum_sent.bits_per_second)/1000000))
+speed_bps=$(iperf3 -c 192.168.1.1 -J | jq .end.sum_sent.bits_per_second)
+speed_mbps=$(echo $speed_bps/1000000 | bc)
 
-if [ $speed -lt 10 ]; then
-	~/pushbullet "Pine Network Slow" "$speed mbits/sec"
+if [ $speed_mbps -lt 10 ]; then
+	~/pushbullet "Pine Network Slow" "$speed_mbps mbits/sec"
 fi
