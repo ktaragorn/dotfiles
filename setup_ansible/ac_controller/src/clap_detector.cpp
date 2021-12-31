b0b5404d677f67134c12c046d6fbf1cc70c19d29
Karthik T <karthikt.holmes+github@gmail.com>
2021-12-31 11:48:11 +0000

    Disable spammy logs from clap detector

--- setup_ansible/ac_controller/src/clap_detector.cpp
+++ setup_ansible/ac_controller/src/clap_detector.cpp
@@ -13,7 +13,7 @@ void clap_setup() {
 void clap_loop() {
   vol.tick();
   clap.tick(vol.getRawMax());
-  mqtt_log("Analog: "+ String(analogRead(A0))+", analyser: " + vol.getRawMax());
+  // mqtt_log("Analog: "+ String(analogRead(A0))+", analyser: " + vol.getRawMax());
   if (clap.isClap()) mqtt_log("clap");
   if (clap.hasClaps(1)) mqtt_log("1 clap");
   if (clap.hasClaps(2)) mqtt_log("2 clap");
