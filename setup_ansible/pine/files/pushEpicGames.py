c8052d5ebc0f96824357e7814435c101caaf38f9
Karthik T <karthikt.holmes+github@gmail.com>
2022-01-02 16:50:31 +0000

    Fix epic script again, and also send to telegram

--- setup_ansible/pine/files/pushEpicGames.py
+++ setup_ansible/pine/files/pushEpicGames.py
@@ -7,23 +7,28 @@ pwd = '/home/ubuntu/'
 def report_error(error):
 	report_cmd = pwd + 'pushbullet "Error with epic games publisher" ' + '"'+str(error)+ '"'
 	subprocess.check_call(report_cmd, shell=True)
+	report_cmd = pwd + 'telegram "Error with epic games publisher : ' +str(error)+ '"'
+	subprocess.check_call(report_cmd, shell=True)
+
 
 def report_free_game(gameName):
-	report_cmd = pwd + 'pushbullet "Epic Games Free game" ' + '"' + gameName + '"' + ' "-d channel_tag=epicgamesfree"'
+	report_cmd = pwd + 'pushbullet "Epic Games Free game(s) " ' + '"' + gameName + '"' + ' "-d channel_tag=epicgamesfree"'
+	subprocess.check_call(report_cmd, shell=True)
+	report_cmd = pwd + 'telegram "Epic Games Free game(s) : '  + gameName + '"'
 	subprocess.check_call(report_cmd, shell=True)
 
 currentFreeGameCmd = "curl https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=en-US | jq '[[.data.Catalog.searchStore.elements[]  | {name: .title,date: .promotions.promotionalOffers[0].promotionalOffers[0].startDate, percent: .promotions.promotionalOffers[0].promotionalOffers[0].discountSetting.discountPercentage}] | .[] |  select(.date != null) | select(.percent == 0) |.name]'"
 
 try:
 	# assuming that if multiple games are offered, all are offered and removed at the same time, so treating as one game.
-	gameData = ",".json.loads(subprocess.check_output(currentFreeGameCmd, shell=True))
+	games = ",".join(json.loads(subprocess.check_output(currentFreeGameCmd, shell=True)))
 	try: 
 		lastReportedGame = open(pwd + "lastEpicFreeGame").read()
 	except FileNotFoundError:
 		lastReportedGame = ""
-	if(gameData['name'] != lastReportedGame):
-		open(pwd + "lastEpicFreeGame", 'w').write(gameData['name'])
-		report_free_game(gameData['name'])
+	if(games != lastReportedGame):
+		open(pwd + "lastEpicFreeGame", 'w').write(games)
+		report_free_game(games)
 
 except Exception as e:
 	logging.info('Error ', exc_info=True)
