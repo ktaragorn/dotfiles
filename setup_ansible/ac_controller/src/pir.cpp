2adc1bf84bad97033da0be5f9d72b0c947a6b768
Karthik T <karthikt.holmes+github@gmail.com>
2022-02-11 11:40:06 +0000

    Push PIR state upon setup

--- setup_ansible/ac_controller/src/pir.cpp
+++ setup_ansible/ac_controller/src/pir.cpp
@@ -2,21 +2,32 @@
 #include "homeassistant_mqtt.h"
 #include "pir.h"
 
-bool current_state = false;
+String current_state = "off";
 
 const uint16_t pirPin = D2;
 
-void pir_setup(){}
-
-void pir_loop(){
+String getPirState() {
 	const bool new_state = digitalRead(pirPin) == 1;
 	const String pir_state = new_state? "on" : "off";
+ 	digitalWrite(LED_BUILTIN, !new_state? HIGH : LOW);
+	return pir_state;
+}
 
- 	// digitalWrite(LED_BUILTIN, !new_state? HIGH : LOW);
+void pushNewState(String new_state){
+	state_homeassistant_mqtt("pir_sensor", new_state);
+	current_state = new_state;
+	mqtt_log("Pir state = " + new_state);
+}
+
+void pir_setup(){
+	pushNewState(getPirState());
+}
+
+void pir_loop(){
+	const String new_state = getPirState();
+
 
 	if(new_state != current_state){
-		state_homeassistant_mqtt("pir_sensor", pir_state);
-		current_state = new_state;
-		mqtt_log("Pir state = " + pir_state);
+		pushNewState(new_state);
 	}
 }
