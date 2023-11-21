0ec69d147d21abbd25675a2f1764f3da8dc62257
Karthik T <karthikt.holmes+github@gmail.com>
2023-11-21 16:49:54 +0000

    Fix home path

--- setup_ansible/pine/scripts/pushEpicGames.py
+++ setup_ansible/pine/scripts/pushEpicGames.py
@@ -2,7 +2,7 @@ import subprocess
 import json
 import logging
 
-pwd = '/Users/karthik1/'
+pwd = '/home/ubuntu/'
 
 logging.basicConfig(level=logging.INFO,format='%(asctime)s [%(levelname)s] %(message)s')
 
