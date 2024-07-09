991e66e067dc30dad512f80d0acda3a33b74e93d
Karthik Thirugnanam <karthik.thirugnanam@thetradedesk.com>
2024-07-09 13:10:33 +0100

    Add windows power shell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+﻿# https://stackoverflow.com/a/62964720
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
--- windows_backup.bat
+++ windows_backup.bat
@@ -1,1 +1,2 @@
 copy  C:\Users\karthik.thirugnanam\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json .
+copy C:\Users\karthik.thirugnanam\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
