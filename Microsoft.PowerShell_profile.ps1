d49553ddd5d8ddfc73ecc7e3a1c1a1006bf21cf0
Karthik Thirugnanam <karthik.thirugnanam@thetradedesk.com>
2024-12-10 14:52:58 +0000

    Emit CWD in powershell so it can be persisted by Windows terminal
    This will ensure that new tabs are in the right folder.
    Changes in settings.json are mostly autochanges.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,20 @@
-﻿# https://stackoverflow.com/a/62964720
+﻿
+# trying https://learn.microsoft.com/en-us/windows/terminal/tutorials/new-tab-same-directory 
+# from https://github.com/dahlbyk/posh-git/issues/501
+function prompt{
+    $prompt = &$GitPromptScriptBlock
+    $loc = $executionContext.SessionState.Path.CurrentLocation;
+
+    $out = ""
+    if ($loc.Provider.Name -eq "FileSystem") {
+        $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+    }
+    
+    return $out + $prompt
+}
 Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+
+# https://stackoverflow.com/a/62964720
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
