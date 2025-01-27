9429a4fc7cfed09f37fb92781e400366ffaa6b77
Karthik Thirugnanam <karthik.thirugnanam@thetradedesk.com>
2025-01-27 15:30:23 +0000

    Backup VSCode settings

---
+++ windows/windows_backup.bat
@@ -0,0 +1,4 @@
+copy  C:\Users\karthik.thirugnanam\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json .
+copy C:\Users\karthik.thirugnanam\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 .
+copy C:\Users\karthik.thirugnanam\AppData\Roaming\Code\User\keybindings.json vscode\
+copy C:\Users\karthik.thirugnanam\AppData\Roaming\Code\User\settings.json vscode\
