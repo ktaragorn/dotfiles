6521e00582c40bae2428e6efe41df5f83bbd4a36
Karthik Thirugnanam <karthik.thirugnanam@thetradedesk.com>
2025-01-31 21:02:52 +0000

    Add powertoys config

--- windows/windows_backup.bat
+++ windows/windows_backup.bat
@@ -1,4 +1,5 @@
 copy  C:\Users\karthik.thirugnanam\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json .
 copy C:\Users\karthik.thirugnanam\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 .
 copy C:\Users\karthik.thirugnanam\AppData\Roaming\Code\User\keybindings.json vscode\
+copy "C:\Users\karthik.thirugnanam\AppData\Local\Microsoft\PowerToys\Keyboard Manager\default.json" .
 copy C:\Users\karthik.thirugnanam\AppData\Roaming\Code\User\settings.json vscode\
